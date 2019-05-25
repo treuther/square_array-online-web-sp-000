30855807f1be2a5e44836527eaf50dc080ad0149
Tracy Reuther <tracyreuther@gmail.com>
2019-05-25 21:07:49 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
-require "pry"
+# require "pry"
 
 def square_array(array)
   array.each do |num|
-    new_array = (num**2)
-    binding.pry
+    new_array = (array[num]**2)
+    # binding.pry
   end
 end
